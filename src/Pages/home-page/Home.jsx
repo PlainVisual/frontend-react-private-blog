c58d71286e5dcca8fdf6ef041c90c31b7dc274e9
PlainVisual <michelvdlinden74@gmail.com>
2023-03-30 10:00:47 +0200

    added some basic styling to the homepage

--- src/Pages/home-page/Home.jsx
+++ src/Pages/home-page/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./homepage.css";
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
               <div className="header-image"></div>
               <div className="header-content">
               <h1>Webdev News</h1>
+              <Link className="nav-link" to="/Login">Go to Login</Link>
               </div>
               
             </main>
